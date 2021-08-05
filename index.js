2cbd92e24fa12e8e5532eb784903b323a1a79449
Renato Boegeholz <renatoboegeholz@gmail.com>
2021-08-05 03:54:07 -0400

    Fixed import statement.

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 require("dotenv").config();
 
-const start = require('./server');
+const server = require('./server')
 const port = process.env.PORT || 3000;
 
 /** Starts the server. */
-start({
+server.start({
     port: port
 }).then(app => {
     console.log("[node] Application is now running on port " + port);
